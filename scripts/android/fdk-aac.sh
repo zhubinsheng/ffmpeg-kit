ba572c7166bcfdc6fe30ba1584b094513c2de833
zhubinsheng <zhubinsheng@yvr.cn>
2023-03-21 11:22:10 +0800

    Create fdk-aac.sh

---
+++ scripts/android/fdk-aac.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ALWAYS CLEAN THE PREVIOUS BUILD
+make distclean 2>/dev/null 1>/dev/null
+
+# REGENERATE BUILD FILES IF NECESSARY OR REQUESTED
+if [[ ! -f "${BASEDIR}"/src/"${LIB_NAME}"/configure ]] || [[ ${RECONF_gmp} -eq 1 ]]; then
+  autoreconf_library "${LIB_NAME}" 1>>"${BASEDIR}"/build.log 2>&1 || return 1
+fi
+
+#--disable-shared 不能禁用动态链接库ffmpeg需要动态库
+AAC_CONFIGURE_FLAGS="--enable-static  --enable-shared  --enable-strip --enable-pic --target=android"
+echo "TOOLCHAINS_PREFIX:$TOOLCHAINS_PREFIX"
+
+./configure \
+  --prefix="${LIB_INSTALL_PREFIX}" \
+  --with-pic \
+  --with-sysroot="${ANDROID_SYSROOT}" \
+  $AAC_CONFIGURE_FLAGS \
+  --host="${HOST}" || return 1
+
+make -j$(get_cpu_count) || return 1
+
+echo "Android aac bulid success!"
+
+make install || return 1
+
+# CREATE PACKAGE CONFIG MANUALLY
+create_fdk-aac_package_config "2.0.2" || return 1
